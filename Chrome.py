6c0a630b33d20ab3fbe898412aa1477a29aae04f
AnnieBrighton <Annie4Brighton@gmail.com>
2023-03-26 22:42:34 +0900

    待ちタイミング変更

--- Chrome.py
+++ Chrome.py
@@ -7,13 +7,15 @@ from subprocess import Popen
 import aiochrome
 import asyncio
 
+
 class Chrome():
     """
     Chrome用ライブラリ
     """
     user_dir = os.path.join(os.environ.get('TEMP', '/tmp'), 'google-chrome_{0:08d}'.format(os.getpid()))
 
-    chrome_app = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    # chrome_app = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    chrome_app = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
 
     def __init__(self, logging) -> None:
         """ コンストラクタ """
@@ -33,7 +35,7 @@ class Chrome():
         opt = [self.chrome_app]
         opt.append('--headless=new')
         opt.append('--user-data-dir=' + self.user_dir)
-        #opt.append('--window-size=800,600')
+        # opt.append('--window-size=800,600')
         opt.append('--window-size=1600,200')
         opt.append('--no-first-run')
         opt.append('--no-default-browser-check')
@@ -44,7 +46,7 @@ class Chrome():
 
         self.proc = Popen(opt)
 
-        sleep(2)
+        sleep(10)
 
         self.browser = aiochrome.Browser(url="http://127.0.0.1:9222")
 
@@ -53,14 +55,13 @@ class Chrome():
             await tab.Network.enable()
             self.idling_tabs.append(tab)
 
-
     async def stop(self) -> None:
         for tab in await self.browser.list_tab():
             await tab.stop()
 
             # wait for loading
             await tab.wait(5)
-        
+
             # close tab
             await self.browser.close_tab(tab)
 
@@ -73,7 +74,6 @@ class Chrome():
         # 起動時に、user_dirが存在しなければ削除する
         shutil.rmtree(self.user_dir)
 
-
     async def open_tab(self):
         if not self.idling_tabs:
             tab = await self.browser.new_tab()
@@ -84,36 +84,32 @@ class Chrome():
 
         return tab
 
-
     async def close_tab(self, tab):
         self.idling_tabs.append(tab)
+
 
 class ChromeTab():
     def __init__(self, browser) -> None:
         self.__browser = browser
         self.__tab = None
 
-
     async def open(self) -> None:
         self.__tab = await self.__browser.open_tab()
-
 
     async def close(self) -> None:
         if self.__tab is not None:
             await self.__browser.close_tab(self.__tab)
             self.__tab = None
 
-  
-    async def get(self, url: str) -> None: 
-        await self.__tab.Page.navigate(url=url, _timeout=10)
+    async def get(self, url: str, timeout=60) -> None:
+        await self.__tab.Page.navigate(url=url, _timeout=timeout)
         await self.__tab.wait(5)
-
 
     async def getDOM(self) -> str:
         for _ in range(0, 10):
             try:
                 root = await self.__tab.DOM.getDocument()
-                HTML = await self.__tab.DOM.getOuterHTML(nodeId=root['root']['nodeId']) 
+                HTML = await self.__tab.DOM.getOuterHTML(nodeId=root['root']['nodeId'])
                 return HTML['outerHTML']
             except aiochrome.exceptions.CallMethodException:
                 await asyncio.sleep(1)
