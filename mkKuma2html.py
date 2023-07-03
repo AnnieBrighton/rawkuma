6f916141836ea10df23532264d1e0c06916b5e05
AnnieBrighton <Annie4Brighton@gmail.com>
2023-07-03 22:05:26 +0900

    パス・ファイル名 URLエンコード

--- mkKuma2html.py
+++ mkKuma2html.py
@@ -5,27 +5,34 @@ import os
 import sys
 from DB import DB
 
+from urllib.parse import unquote
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(threadName)s: %(message)s', filename='rawkuma.log')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(threadName)s: %(message)s",
+    filename="rawkuma.log",
+)
 console = logging.StreamHandler()
-console.setFormatter(logging.Formatter('%(asctime)s %(threadName)s: %(message)s'))
-logging.getLogger('').addHandler(console)
+console.setFormatter(logging.Formatter("%(asctime)s %(threadName)s: %(message)s"))
+logging.getLogger("").addHandler(console)
+
 
 class MkKuma2Html:
-    BASE_PATH = 'Books'
+    BASE_PATH = "Books"
 
     def __init__(self) -> None:
-        self.db = DB(logging)        
+        self.db = DB(logging)
         self.path = os.path.abspath(os.path.dirname(sys.argv[0]))
-        self.lookup = TemplateLookup(filesystem_checks=False, directories=[self.path + "/templates"])
+        self.lookup = TemplateLookup(
+            filesystem_checks=False, directories=[self.path + "/templates"]
+        )
 
     def tohtml(self) -> None:
-        """ HTML出力
-        """
+        """HTML出力"""
         lists = self.db.select_book(**{DB.BOOK_TYPE: None})
-        types = sorted(set([ val[DB.BOOK_TYPE] for val in lists ]))
+        types = sorted(set([val[DB.BOOK_TYPE] for val in lists]))
 
         # ベースディレクトリ作成
         os.makedirs(self.BASE_PATH, exist_ok=True)
@@ -34,7 +41,15 @@ class MkKuma2Html:
         self.output_book(types)
 
         for type in types:
-            books = self.db.select_book(**{DB.BOOK_KEY: None, DB.TITLE: None, DB.BOOK_TYPE: type, DB.THUMB: None, DB.USE_FLAG: None})
+            books = self.db.select_book(
+                **{
+                    DB.BOOK_KEY: None,
+                    DB.TITLE: None,
+                    DB.BOOK_TYPE: type,
+                    DB.THUMB: None,
+                    DB.USE_FLAG: None,
+                }
+            )
 
             # ブック一覧HTML出力
             self.output_book_type(books, types, type)
@@ -42,7 +57,11 @@ class MkKuma2Html:
             for book in books:
                 chapters = self.db.select_chapter(book[DB.BOOK_ID])
 
-                path = os.path.join(self.BASE_PATH, 'img{TYPE}'.format(TYPE=book[DB.BOOK_TYPE]), book[DB.BOOK_KEY])
+                path = unquote(os.path.join(
+                    self.BASE_PATH,
+                    "img{TYPE}".format(TYPE=book[DB.BOOK_TYPE]),
+                    book[DB.BOOK_KEY],
+                ))
 
                 # チャプター格納ディレクトリ作成
                 os.makedirs(path, exist_ok=True)
@@ -54,21 +73,20 @@ class MkKuma2Html:
                 self.output_chapter(book, path, chapters)
 
     def output_book(self, types):
-        """ トップの Books.html を作成
+        """トップの Books.html を作成
 
         Args:
             types ( list ): タイプのリスト
         """
 
         tmpl = self.lookup.get_template("Books.html")
-        data = {'types': [type for type in types]}
- 
-        with open(os.path.join(self.BASE_PATH, 'Books.html'), mode='w') as f:
+        data = {"types": [type for type in types]}
+
+        with open(os.path.join(self.BASE_PATH, "Books.html"), mode="w") as f:
             f.write(tmpl.render(**data))
 
-
     def output_book_type(self, books, types, type):
-        """ タイプのブック一覧表示
+        """タイプのブック一覧表示
 
         Args:
             books (_type_): ブックリスト
@@ -78,20 +96,28 @@ class MkKuma2Html:
 
         tmpl = self.lookup.get_template("Books_type.html")
 
-        data = {'types': [type for type in types],
-                'cur_type': type,
-                'books': [
-                    {'HREF': os.path.join('img{TYPE}'.format(TYPE=book[DB.BOOK_TYPE]), book[DB.BOOK_KEY], book[DB.BOOK_KEY] + ".html"),
-                     'SRC': book[DB.THUMB],
-                     'FLAG': book[DB.USE_FLAG],
-                     'TITLE': book[DB.TITLE]
-                    } for book in books
-                ]
-            }
+        data = {
+            "types": [type for type in types],
+            "cur_type": type,
+            "books": [
+                {
+                    "HREF": os.path.join(
+                        "img{TYPE}".format(TYPE=book[DB.BOOK_TYPE]),
+                        book[DB.BOOK_KEY],
+                        book[DB.BOOK_KEY] + ".html",
+                    ),
+                    "SRC": book[DB.THUMB],
+                    "FLAG": book[DB.USE_FLAG],
+                    "TITLE": book[DB.TITLE],
+                }
+                for book in books
+            ],
+        }
 
-        with open(os.path.join(self.BASE_PATH, 'Books{TYPE}.html'.format(TYPE=type)), mode='w') as f:
+        with open(
+            os.path.join(self.BASE_PATH, "Books{TYPE}.html".format(TYPE=type)), mode="w"
+        ) as f:
             f.write(tmpl.render(**data))
-
 
     def output_chapter_top(self, type, book, path, chapters):
         """_summary_
@@ -105,21 +131,28 @@ class MkKuma2Html:
 
         tmpl = self.lookup.get_template("Chapter_top.html")
 
-        data = {'type': type,
-                'thumb': book[DB.THUMB],
-                'title': book[DB.TITLE],
-                'book_key': book[DB.BOOK_KEY],
-                'chapters': [
-                    {'KEY': chap[DB.CHAPTER_KEY],
-                    'HREF': book[DB.BOOK_KEY] + '_' + chap[DB.CHAPTER_KEY] + '.html',
-                    'NUM': chap[DB.CHAPTER_NUM],
-                    'DATE': chap[DB.CHAPTER_DATE].strftime('%Y年%m月%d日')
-                    } for chap in sorted(chapters, key=lambda x: x[DB.CHAPTER_KEY], reverse=True)
-                ]}
+        data = {
+            "type": type,
+            "thumb": book[DB.THUMB],
+            "title": book[DB.TITLE],
+            "book_key": book[DB.BOOK_KEY],
+            "chapters": [
+                {
+                    "KEY": chap[DB.CHAPTER_KEY],
+                    "HREF": book[DB.BOOK_KEY] + "_" + chap[DB.CHAPTER_KEY] + ".html",
+                    "NUM": chap[DB.CHAPTER_NUM],
+                    "DATE": chap[DB.CHAPTER_DATE].strftime("%Y年%m月%d日"),
+                }
+                for chap in sorted(
+                    chapters, key=lambda x: x[DB.CHAPTER_KEY], reverse=True
+                )
+            ],
+        }
 
-        with open(os.path.join(path, book[DB.BOOK_KEY] + '.html'), mode='w') as f:
+        with open(
+            unquote(os.path.join(path, book[DB.BOOK_KEY] + ".html")), mode="w"
+        ) as f:
             f.write(tmpl.render(**data))
-
 
     def output_chapter(self, book, path, chapters):
         """_summary_
@@ -134,28 +167,49 @@ class MkKuma2Html:
 
         max = len(chapters)
         for index, chapter in enumerate(chapters):
-
             prev = chapters[index - 1]
             next = chapters[(index + 1) % max]
 
             pages = self.db.select_page(chapter[DB.CHAPTER_ID])
 
-            data = {'prev': {'HREF': book[DB.BOOK_KEY] + '_' + prev[DB.CHAPTER_KEY] + '.html', 'NUM': prev[DB.CHAPTER_NUM]},
-                    'next': {'HREF': book[DB.BOOK_KEY] + '_' + next[DB.CHAPTER_KEY] + '.html', 'NUM': next[DB.CHAPTER_NUM]},
-                    'book_key': book[DB.BOOK_KEY],
-                    'chapter_key': chapter[DB.CHAPTER_KEY],
-                    'title': book[DB.TITLE],
-                    'chapter_top': book[DB.BOOK_KEY] + '.html',
-                    'chapters': [
-                        {'KEY': chap[DB.CHAPTER_KEY],
-                         'HREF': book[DB.BOOK_KEY] + '_' + chap[DB.CHAPTER_KEY] + '.html',
-                         'NUM': chap[DB.CHAPTER_NUM]
-                        } for chap in sorted(chapters, key=lambda x: x[DB.CHAPTER_KEY], reverse=True)
-                    ],
-                    'pages': [ page['page_url'] for page in pages ]
-                }
+            data = {
+                "prev": {
+                    "HREF": book[DB.BOOK_KEY] + "_" + prev[DB.CHAPTER_KEY] + ".html",
+                    "NUM": prev[DB.CHAPTER_NUM],
+                },
+                "next": {
+                    "HREF": book[DB.BOOK_KEY] + "_" + next[DB.CHAPTER_KEY] + ".html",
+                    "NUM": next[DB.CHAPTER_NUM],
+                },
+                "book_key": book[DB.BOOK_KEY],
+                "chapter_key": chapter[DB.CHAPTER_KEY],
+                "title": book[DB.TITLE],
+                "chapter_top": book[DB.BOOK_KEY] + ".html",
+                "chapters": [
+                    {
+                        "KEY": chap[DB.CHAPTER_KEY],
+                        "HREF": book[DB.BOOK_KEY]
+                        + "_"
+                        + chap[DB.CHAPTER_KEY]
+                        + ".html",
+                        "NUM": chap[DB.CHAPTER_NUM],
+                    }
+                    for chap in sorted(
+                        chapters, key=lambda x: x[DB.CHAPTER_KEY], reverse=True
+                    )
+                ],
+                "pages": [page["page_url"] for page in pages],
+            }
 
-            with open(os.path.join(path, book[DB.BOOK_KEY] + '_' + chapter[DB.CHAPTER_KEY] + '.html'), mode='w') as f:
+            with open(
+                unquote(
+                    os.path.join(
+                        path,
+                        book[DB.BOOK_KEY] + "_" + chapter[DB.CHAPTER_KEY] + ".html",
+                    )
+                ),
+                mode="w",
+            ) as f:
                 f.write(tmpl.render(**data))
 
 
@@ -163,7 +217,7 @@ class MkKuma2Html:
 # メイン
 #
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         url = sys.argv[1]
 
